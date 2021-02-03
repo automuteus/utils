6a790f06e7a29669f278ff24c1e7bcaf471ecda9
denverquane <chewysnarf@gmail.com>
2021-02-02 21:00:58 -0700

    Added isExpired method for re-use

--- pkg/premium/premium.go
+++ pkg/premium/premium.go
@@ -20,7 +20,14 @@ var TierStrings = []string{
 	"SelfHost",
 }
 
+const SubDays = 31         // use 31 because there shouldn't ever be a gap; whenever a renewal happens on the 31st day, that should be valid
+const NoExpiryCode = -9999 // dumb, but no one would ever have expired premium for 9999 days
+
 type PremiumRecord struct {
 	Tier Tier `json:"tier"`
 	Days int  `json:"days"`
+}
+
+func IsExpired(tier Tier, days int) bool {
+	return tier == FreeTier || (days != NoExpiryCode && days < 1)
 }
